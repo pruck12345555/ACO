041c34df00188e20be2c8e32aa84875595f49a19
Pruck <Deecufs@gmail.com>
2025-11-16 03:59:10 +0700

    before adding random

--- acoParallel.cu
+++ acoParallel.cu
@@ -17,8 +17,8 @@ struct edges{
 __global__ void travel(int totalNodes, edges* map, float* delta){
     const int gidx = blockIdx.x * blockDim.x + threadIdx.x;
     bool visited[TOTAL_NODE];
-    bool temp[TOTAL_NODE][TOTAL_NODE]; // Boolean matrix to store edges that
-    // kth ant went through
+    bool temp[TOTAL_NODE][TOTAL_NODE]; // Boolean matrix to store edges that kth ant went through
+
     // Inititalize temp
     for(int i = 0; i < TOTAL_NODE; i++){
         for(int j = 0; j < TOTAL_NODE; j++){
@@ -32,11 +32,40 @@ __global__ void travel(int totalNodes, edges* map, float* delta){
 
     // Inititalize visited array for each ant
     for(int i = 0; i < totalNodes; i++) visited[i] = 0;
+    visited[0] = true;
 
     // Ant kth travel thorugh every node
     if(gidx < TOTAL_ANT){
         for(int i = 0; i < totalNodes - 1; i++){
             // Calculate for next node
+            // Getting totalProbs
+            float probs[TOTAL_NODE];
+            float totalProbs = 0;
+            for(int l = 0; l < totalNodes; l++) probs[l] = 0;
+
+            for(int j = 0; j < totalNodes; j++){
+                if(!visited[j] && map[currNode*totalNodes+j].cost > 0){ // connects
+                    float Tij = map[currNode*totalNodes+j].pheromone;
+                    float nij = 1.0 / map[currNode*totalNodes+j].cost; 
+                    probs[j] = Tij * nij; 
+                    totalProbs += Tij * nij;
+                }
+            }
+
+            // Getting all probabilities
+            for(int k = 0; k < totalNodes; k++){
+                if(probs[k] > 0){
+                    probs[k] = probs[k] / totalProbs;
+                }
+            }
+
+            // Turning probs to cumulative sum
+
+
+            // Random and choose node
+            
+
+            // Get next node
             totalCost += map[currNode*totalNodes + nextNode].cost;
             temp[currNode][nextNode] = true;
             temp[nextNode][currNode] = true;
@@ -67,7 +96,9 @@ __global__ void updatePheromones(int totalNodes, float evaRate, edges* map, floa
     const int row = blockDim.y * blockIdx.y + threadIdx.y;
     const int gidx = row*totalNodes + col;
     
-    map[gidx].pheromone = (1-evaRate) * map[gidx].pheromone + delta[gidx];
+    if(col < TOTAL_NODE && row < TOTAL_NODE){
+        map[gidx].pheromone = (1-evaRate) * map[gidx].pheromone + delta[gidx];
+    }
 }
 
 void ACOParallel(int totalNodes, float evaRate, size_t mapSize, edges* map, int epochs){
@@ -78,8 +109,8 @@ void ACOParallel(int totalNodes, float evaRate, size_t mapSize, edges* map, int 
     dim3 dimBlock(10,10,1);
 
     // Allocate for device map and delta matrix
-    cudaMalloc((void**) d_map, mapSize);
-    cudaMalloc((void**) delta, totalNodes*totalNodes*sizeof(float));
+    cudaMalloc((void**) &d_map, mapSize);
+    cudaMalloc((void**) &delta, totalNodes*totalNodes*sizeof(float));
 
     // Copy host to device and travel
     cudaMemcpy(d_map, map, mapSize, cudaMemcpyHostToDevice);
